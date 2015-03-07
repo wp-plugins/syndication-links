7917d5ff0fd8c55d97487396abb29c10b4790ae8
dshanske <dshanske@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-07 22:26:52 +0000

    Version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/syndication-links/trunk@1107811 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- syn-display.php
+++ syn-display.php
@@ -32,7 +32,13 @@ function get_syndication_links() {
       else {
         $name = $domain;
       }
-      $synlinks .=  '<li><a title="' . $name . '" class="u-syndication" href="' . esc_url($url) . '" rel="syndication">'; 
+      $synlinks .=  '<li><a title="' . $name . '" class="u-syndication" href="' . esc_url($url) . '"';
+      if (is_single() ) {
+        $synlinks .= ' rel="syndication">'; 
+      }
+      else {
+        $synlinks .= '>';
+      }
       if ($options['just_icons'] == "1") {
         $synlinks .= $name;
       }
@@ -52,7 +58,7 @@ function syndication_links_after($meta = "" ) {
    }
 
 $option = get_option('syndication_content_options');
-if($option['the_content']=="0"){
+if($option['the_content']!="1"){
          add_filter( 'the_content', 'syndication_links_after', 20 );
    }
 
